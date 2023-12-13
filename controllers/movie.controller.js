5ecaaf7dad7b5a5be51e48f631af78e9945ac1ad
SinekaRamu <sineka2001cud@gmail.com>
2023-12-13 23:11:49 +0530

    get movies & single movie

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,4 +1,4 @@
-const { models } = require("../config/sequalize-config");
+const { models, Sequelize } = require("../config/sequalize-config");
 
 const addMoviesController = async (req, res, next) => {
   try {
@@ -21,14 +21,18 @@ const addMoviesController = async (req, res, next) => {
 
 const getMoviesController = async (req, res, next) => {
   try {
-    const getMovies = await models.users.findOne({
-      where: {
-        user_id: req.decoded.id,
-      },
-      returning: true,
+    const getMovies = await models.movies.findAll();
+
+    const overallRating = await models.ratings.findAll({
+      attributes: [
+        "movie_id",
+        [Sequelize.fn("AVG", Sequelize.col("rating")), "overall_rating"],
+      ],
+      logging: true,
+      group: ["movie_id"],
     });
 
-    res.json(getMovies);
+    res.json({ getMovies, overallRating });
   } catch (error) {
     return next({
       status: 400,
@@ -37,7 +41,39 @@ const getMoviesController = async (req, res, next) => {
   }
 };
 
+const getMovieController = async (req, res, next) => {
+  try {
+    const getMovie = await models.movies.findOne({
+      where: { id: req.params.id },
+      include: [
+        {
+          association: "rating",
+          attributes: ["rating"],
+          include: [
+            {
+              association: "userRating",
+              attributes: ["user_name"],
+            },
+          ],
+        },
+        {
+          association: "addedBy",
+          attributes: ["first_name"],
+        },
+      ],
+      logging: true,
+    });
+
+    res.json(getMovie);
+  } catch (error) {
+    return next({
+      status: 400,
+      message: error.message,
+    });
+  }
+};
 module.exports = {
   addMoviesController,
   getMoviesController,
+  getMovieController,
 };
